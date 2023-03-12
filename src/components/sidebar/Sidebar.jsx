023c143c85ef9d1eea785fcaa82df011560f2d19
Op Pautu <pautuop@gmail.com>
2023-03-12 15:38:40 +0530

    Finish sidebar component and style

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,40 @@
 import "./Sidebar.css";
 
 export default function Sidebar() {
-  return <div className="sidebar">sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <p className="about-text">
+          Hi, my name is Amelie. I do life coaching, I play the piano, and I
+          love playing sports. I am also a designer and a tech enthusiast. Not
+          to mention my love for cinema.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Design</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
