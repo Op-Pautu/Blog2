209975d2a9dd5b5fc295b7fcc93c74956e4a9ee2
Op Pautu <pautuop@gmail.com>
2023-03-12 21:15:39 +0530

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import TopBar from "./components/topbar/TopBar";
-import Write from "./components/write/Write";
+import Write from "./pages/write/Write";
 import Home from "./pages/home/Home";
 
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Login />
     </>
   );
 }
