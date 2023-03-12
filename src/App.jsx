519d6b8072338852a81e445ac54ca88181d0be4e
Op Pautu <pautuop@gmail.com>
2023-03-12 15:24:04 +0530

    Add sidebar and posts in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import TopBar from "../components/topbar/TopBar";
-import Home from "../pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
