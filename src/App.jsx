236dab68e9844ad41348bf1e81f4e595cd2a40a5
Op Pautu <pautuop@gmail.com>
2023-03-12 17:48:07 +0530

    Finish settings page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import TopBar from "./components/topbar/TopBar";
 import Write from "./components/write/Write";
 import Home from "./pages/home/Home";
-import Single from "./pages/home/single/Single";
+
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
     </>
   );
 }
