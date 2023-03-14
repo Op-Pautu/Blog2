9c526544c30e787d6847c74ed0aa425732cf94a1
Op Pautu <pautuop@gmail.com>
2023-03-15 01:46:24 +0530

    Finish api

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
+
+//routes
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -17,10 +22,24 @@ mongoose
     console.log(err);
   });
 
-//routes
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "Rectangle.png");
+  },
+});
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+//middleware
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
