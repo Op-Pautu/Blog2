82f3219fd4a9bbc412b96f315b582f4e56791cf8
Op Pautu <pautuop@gmail.com>
2023-03-17 13:44:12 +0530

    Use context api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "./context/Context";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,17 +1,38 @@
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./Login.css";
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("localhost:5000/api/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS" });
+    } catch (err) {}
+  };
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input type="text" className="loginInput" required />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input type="text" className="loginInput" required ref={userRef} />
         <label>Password</label>
-        <input type="password" className="loginInput" required />
+        <input
+          type="password"
+          className="loginInput"
+          required
+          ref={passwordRef}
+        />
         <button className="loginButton">Login</button>
       </form>
-      <button className="loginRegisterButton">
+      <button className="loginRegisterButton" type="submit">
         <Link to="/register" className="link">
           Register
         </Link>
